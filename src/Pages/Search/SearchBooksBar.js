00f837485f7d00b7f92d82888a4aa119177982a5
Rabii Luena <rbluena@gmail.com>
2020-03-24 11:15:37 +0300

    fix: back button throw exception when clicked
    Back button was using "this" keyword inside SearchBooksBar component while the component is not
    classical. This lead into an error when the button clicked.

--- src/Pages/Search/SearchBooksBar.js
+++ src/Pages/Search/SearchBooksBar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import SearchInput from "./SearchInput";
-import Button from "./../../Components/Elements/Button";
+import Button from "../../Components/Elements/Button";
 
-const SearchBooksBar = () => {
+const SearchBooksBar = ({ history }) => {
   return (
     <div className="search-books-bar">
-      <Button onClick={() => this.props.history.push("/")} modifiers={["back"]}>
+      <Button onClick={() => history.push("/")} modifiers={["back"]}>
         Close
       </Button>
       <SearchInput />
